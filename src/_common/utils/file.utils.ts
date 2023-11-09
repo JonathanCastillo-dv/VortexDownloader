0661626d11f141097f77c50bf3cda71a709129de
@jonathan.castillo2 <jonathan.castillo@naranjax.com>
2023-11-09 20:11:55 -0300

    fix: folder public

--- src/_common/utils/file.utils.ts
+++ src/_common/utils/file.utils.ts
@@ -1,5 +1,23 @@
+import * as fs from 'fs';
+
 type FileLink = { name: string; url: string };
 type FileLinksArray = FileLink[];
+
+
+
+
+const validateAndCreatePath = (directoryPath: string) => {
+    try {
+        if (!fs.existsSync(directoryPath)) {
+            fs.mkdirSync(directoryPath, { recursive: true });
+            console.log(`Path '${directoryPath}' created successfully.`.bgYellow);
+        } else {
+            console.log(`Path '${directoryPath}' already exists.`.bgYellow);
+        }
+    } catch (error) {
+        console.error(`Error validating/creating the path '${directoryPath}': ${error}`.bgRed);
+    }
+}
 
 const generateFileUrls = (host: string | string[] | undefined, filenames: string[]): FileLinksArray => {
     return filenames.map((filename) => {
@@ -11,5 +29,6 @@ const generateFileUrls = (host: string | string[] | undefined, filenames: string
 };
 
 export {
-    generateFileUrls
+    generateFileUrls,
+    validateAndCreatePath
 }
